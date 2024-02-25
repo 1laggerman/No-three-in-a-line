6e6745206597b32ea1b87799cbff39e4cbec0b24
1laggerman <121728629+1laggerman@users.noreply.github.com>
2024-02-25 11:08:15 +0200

    started on valid points set

---
+++ package/randomSet.py
@@ -0,0 +1,19 @@
+import random
+from Point import Point
+import numpy as np
+
+class validPoints():
+    m = np.ndarray
+    s = set()
+    
+    def __init__(self, points: list[Point]):
+        self.s = points
+        self.l = points
+        
+    def __len__(self):
+        return self.l.__len__()
+    
+    def remove(self, point: Point):
+        p = self.s.
+    
+        
